11de79c27a3658269c0059e797d4acc5e3c8913f
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2026-02-01 17:39:32 -0500

    added color blobs

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Intro from './components/Intro';
@@ -13,32 +13,23 @@ import Design from './pages/Design.jsx';
 import Playground from './pages/Playground.jsx';
 import ColorBlobs from './components/shared/ColorBlobs';
 
-function AppContent() {
-  const location = useLocation();
-  const isIntroPage = location.pathname === '/';
-
-  return (
-    <div className="App">
-      {!isIntroPage && <ColorBlobs />}
-      <Header />
-      <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/code" element={<Code />} />
-        <Route path="/design" element={<Design />} />
-        <Route path="/playground" element={<Playground />} />
-        <Route path="/sunney" element={<Sunney />} />
-        <Route path="/spotify" element={<Spotify />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
-}
-
 function App() {
   return (
     <Router>
-      <AppContent />
+      <div className="App">
+        <ColorBlobs />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/code" element={<Code />} />
+          <Route path="/design" element={<Design />} />
+          <Route path="/playground" element={<Playground />} />
+          <Route path="/sunney" element={<Sunney />} />
+          <Route path="/spotify" element={<Spotify />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
